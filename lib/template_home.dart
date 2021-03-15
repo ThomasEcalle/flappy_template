f016078ffe71adc0043932ff8ce66ed28a661a2a
Thomas Ecalle <thomasecalle@gmail.com>
2021-03-15 22:01:35 +0100

    Initial commit

---
+++ lib/template_home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TemplateHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Hello from Flappy Template"),
+      ),
+    );
+  }
+}
