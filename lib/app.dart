e95bdf37a3d23b4b72e4ea91f53b17b923d91fef
Thomas Ecalle <thomasecalle@gmail.com>
2021-03-16 00:39:12 +0100

    Add template app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,7 @@
 import 'package:flappy_template/template_home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
